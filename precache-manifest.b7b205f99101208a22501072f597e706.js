386e14549f169ce135feb8fb744faa3690565025
Mike Berman <mike@berman.xyz>
2019-01-27 17:16:40 +0000

    Updates

---
+++ precache-manifest.b7b205f99101208a22501072f597e706.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "915cd99ade46abec8bf9",
+    "url": "/tents-and-trees/static/js/main.915cd99a.chunk.js"
+  },
+  {
+    "revision": "8bad3a740b9ec2e992fe",
+    "url": "/tents-and-trees/static/js/1.8bad3a74.chunk.js"
+  },
+  {
+    "revision": "da12d035489ce4d6abfa",
+    "url": "/tents-and-trees/static/js/runtime~main.da12d035.js"
+  },
+  {
+    "revision": "8b5e71d0ea068d0f9ed7de82cab0aa62",
+    "url": "/tents-and-trees/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tents-and-trees/precache-manifest.a6f7f8c6f55233c859ebddd952f8151c.js"
+  "/tents-and-trees/precache-manifest.b7b205f99101208a22501072f597e706.js"
 );
 
 workbox.clientsClaim();
